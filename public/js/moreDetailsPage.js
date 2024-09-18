c366e23dd1f28841c54015460b4e88286906c183
leonel-pala <linderman343@gmail.com>
2024-09-18 01:56:49 -0300

    Primer lanzamiento

--- public/js/moreDetailsPage.js
+++ public/js/moreDetailsPage.js
@@ -8,9 +8,9 @@ const details = {
     description:
       "<b>Twitter Trucho</b> es una aplicación web diseñada para replicar las principales funcionalidades de Twitter, ofreciendo una experiencia de interacción y comunicación en redes sociales. Actualmente, la aplicación permite a los usuarios crear y gestionar su perfil, publicar 'tweets', y dar o quitar 'me gusta' a los tweets que les interesen. Los usuarios también pueden ver un historial de todos los tweets a los que han dado 'me gusta'. En el futuro, se planea añadir funciones como la gestión de comentarios en los tweets, la edición de tweets propios, y la posibilidad de seguir a otros usuarios, entre otras mejoras.",
     urlImg: [
-      './public/demo/twitter-trucho.png',
-      './public/demo/carousel/twitter-trucho/img2.png',
-      './public/demo/carousel/twitter-trucho/img3.png',
+      './demo/twitter-trucho.png',
+      './demo/carousel/twitter-trucho/img2.png',
+      './demo/carousel/twitter-trucho/img3.png',
     ],
   },
   'rincon-de-la-cordillera': {
@@ -22,13 +22,13 @@ const details = {
     description:
       '<b>Rincón de la Cordillera</b> es una web app diseñada para un complejo de cabañas ubicado en Esquel. Esta plataforma permite a los usuarios realizar pedidos y gestionar reservas de manera eficiente. Los clientes pueden explorar las opciones de alojamiento, realizar reservas en tiempo real y gestionar sus reservas existentes. La aplicación está orientada a ofrecer una experiencia de usuario fluida y accesible, facilitando la administración de reservas y mejorando la comunicación entre los clientes y el complejo de cabañas.',
     urlImg: [
-      './public/demo/rincon-de-la-cordillera.png',
-      './public/demo/carousel/rincon-de-la-cordillera/img2.png',
-      './public/demo/carousel/rincon-de-la-cordillera/img3.png',
-      './public/demo/carousel/rincon-de-la-cordillera/img4.png',
-      './public/demo/carousel/rincon-de-la-cordillera/img5.png',
-      './public/demo/carousel/rincon-de-la-cordillera/img6.png',
-      './public/demo/carousel/rincon-de-la-cordillera/img7.png',
+      './demo/rincon-de-la-cordillera.png',
+      './demo/carousel/rincon-de-la-cordillera/img2.png',
+      './demo/carousel/rincon-de-la-cordillera/img3.png',
+      './demo/carousel/rincon-de-la-cordillera/img4.png',
+      './demo/carousel/rincon-de-la-cordillera/img5.png',
+      './demo/carousel/rincon-de-la-cordillera/img6.png',
+      './demo/carousel/rincon-de-la-cordillera/img7.png',
     ],
   },
   'mensajeria-pala': {
@@ -40,8 +40,8 @@ const details = {
     description:
       '<b>Mensajería Pala</b> es una aplicación web de mensajería diseñada para facilitar la comunicación en tiempo real entre usuarios, ofreciendo una experiencia de chat fluida y eficiente. La interfaz es intuitiva y sencilla, lo que permite a los usuarios enviar y recibir mensajes de manera instantánea, sin complicaciones. Al utilizar tecnologías modernas como <b>WebSocket</b> para la transmisión en tiempo real y <b>Turso</b> para el almacenamiento de datos, la aplicación garantiza una comunicación rápida y fiable. Mensajería Pala está diseñada para ser ligera y accesible, proporcionando una herramienta de comunicación efectiva tanto para conversaciones informales como para entornos profesionales.',
     urlImg: [
-      './public/demo/carousel/mensajeria-pala/img2.png',
-      './public/demo/mensajeria-pala.png',
+      './demo/carousel/mensajeria-pala/img2.png',
+      './demo/mensajeria-pala.png',
     ],
   },
   tetris: {
@@ -52,7 +52,7 @@ const details = {
       'El desarrollo de <b>Tetris Épico</b> me brindó la oportunidad de profundizar en el uso de la etiqueta <b>canvas</b> en HTML, una herramienta poderosa para la creación de gráficos dinámicos y juegos interactivos en el navegador. A través de este proyecto, aprendí cómo manipular directamente los píxeles en el canvas para representar los bloques de Tetris y cómo actualizar eficientemente la pantalla en cada movimiento. Además, comprendí el valor de utilizar <b>matrices</b> para gestionar el estado del juego. Las matrices permitieron representar la cuadrícula del juego de Tetris, facilitando la detección de colisiones, la rotación de piezas y la lógica de eliminación de líneas completas. Este enfoque también mejoró mi habilidad para manejar estructuras de datos y optimizar el rendimiento del juego al reducir la cantidad de operaciones necesarias para cada actualización del estado del juego.',
     description:
       '<b>Tetris Épico</b> es una página web diseñada para replicar las funcionalidades del icónico juego Tetris, ofreciendo una experiencia de juego clásica con un toque moderno. La aplicación permite a los jugadores disfrutar del mismo desafío de apilar bloques y completar líneas, todo desde la comodidad de su navegador web. Al aprovechar las capacidades de HTML5 y JavaScript, la página ofrece una jugabilidad fluida y una interfaz intuitiva, manteniendo la esencia del juego original mientras explora nuevas posibilidades de diseño y usabilidad.',
-    urlImg: ['./public/demo/tetris.png'],
+    urlImg: ['./demo/tetris.png'],
   },
   'ecomerce-olimpiadas': {
     title: 'Ecomerce olimpiadas',
@@ -63,11 +63,11 @@ const details = {
     description:
       '<b>Tetris Épico</b> es una página web diseñada para replicar las funcionalidades del icónico juego Tetris, ofreciendo una experiencia de juego clásica con un toque moderno. La aplicación permite a los jugadores disfrutar del mismo desafío de apilar bloques y completar líneas, todo desde la comodidad de su navegador web. Al aprovechar las capacidades de HTML5 y JavaScript, la página ofrece una jugabilidad fluida y una interfaz intuitiva, manteniendo la esencia del juego original mientras explora nuevas posibilidades de diseño y usabilidad.',
     urlImg: [
-      './public/demo/ecomerce-olimpiadas.png',
-      './public/demo/carousel/ecomerce-olimpiadas/img2.png',
-      './public/demo/carousel/ecomerce-olimpiadas/img3.png',
-      './public/demo/carousel/ecomerce-olimpiadas/img4.png',
-      './public/demo/carousel/ecomerce-olimpiadas/img5.png',
+      './demo/ecomerce-olimpiadas.png',
+      './demo/carousel/ecomerce-olimpiadas/img2.png',
+      './demo/carousel/ecomerce-olimpiadas/img3.png',
+      './demo/carousel/ecomerce-olimpiadas/img4.png',
+      './demo/carousel/ecomerce-olimpiadas/img5.png',
     ],
   },
 };
